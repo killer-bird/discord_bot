5cc8c0a9092468c21282c492ba95fa9c411b601e
killer-bird <63458405+React-Mechanicus@users.noreply.github.com>
2022-06-26 02:42:44 +0300

    refactor btn handler

--- src/deploy-buttons.ts
+++
@@ -1,8 +0,0 @@
-import * as buttonsModules from "./buttons";
-import { IButton } from "./interfaces/IButton";
-
-const buttons = []
-
-for (const module of Object.values<any>(buttonsModules)) {   
-    buttons.push(module.data)
-}
